1183e940122a09e9653e670cdcc20ffe30fa40b8
Rocket Programmer <me@rocketprogrammer.me>
2022-05-05 21:11:15 -0500

    setup-basic: Remove subinacl hash checking for now

--- vagranttools/setup_basic.ps1
+++ vagranttools/setup_basic.ps1
@@ -46,18 +46,11 @@ $regdiffArchivePath="C:\Windows\Temp\$regdiffArchive"
 $regdiffUrl="http://p-nand-q.com/download/$regdiffArchive"
 echo $regdiffUrl
 (New-Object System.Net.WebClient).DownloadFile($regdiffUrl, $regdiffArchivePath)
-$regdiffHash = (Get-FileHash $regdiffArchivePath -Algorithm MD5).Hash
-$regdiffExpectedHash = "E5F910DA1EF3402653EAB4D4D8DC428F"
-echo checking hashes $regdiffHash =? $regdiffExpectedHash
-If ($regdiffHash -eq $regdiffExpectedHash) {
-    7z x $regdiffArchivePath
-    cp $regdiffName/* C:\ProgramData\chocolatey\bin\
-    rm -r -fo $regdiffArchivePath
-    rm -r -fo $regdiffName
-    which regdiff
-}  Else {
-    echo "ERROR: regdiff hash doesn't match!"
-}
+7z x $regdiffArchivePath
+cp $regdiffName/* C:\ProgramData\chocolatey\bin\
+rm -r -fo $regdiffArchivePath
+rm -r -fo $regdiffName
+which regdiff
 
 # subinacl (sadly not in choco)
 $subinaclName="subinacl.msi"
